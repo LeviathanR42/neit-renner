020ab31b627c0ab033d88574fd5ded276190191d
LeviathanR42 <rfrenner@email.neit.edu>
2024-11-07 10:38:46 -0500

    w6s2

---
+++ w6/w5_Game_Start_Files/js/game.js
@@ -0,0 +1,130 @@
+/*-------------------------------------------
+Game Setup
+ 1. canvas 
+ 2. context
+ 3. frame rate
+ 4. animation timer runs main function 60 frames per second
+-------------------------------------------*/
+var c = document.querySelector(`canvas`)
+var ctx = c.getContext(`2d`)
+var fps = 1000/60
+var timer = setInterval(main, fps)
+
+
+/*------------Declare Variables Here--------*/
+
+var avatar = new GameObject();
+avatar.color = `#ff0099`;
+avatar.vx = 2;
+avatar.vy = 2;
+
+var amt = 30;
+var sheep = [];
+for(var i=0; i<amt; i++)
+    {
+       console.log(`baaa! ${i}`)
+       sheep[i] = new GameObject();
+       sheep[i].color = `#000000`;
+       sheep[i].w = 18; 
+       sheep[i].h = 18;
+       sheep[i].x = rand(0, c.width);
+       sheep[i].y = rand(0, c.height);
+        
+        while(sheep[i].isOverPoint(avatar))
+        {
+            if(sheep[i].x < avatar.x)
+            {
+               sheep[i].x-=1;
+            }
+            if(sheep[i].x >= avatar.x)
+            {
+               sheep[i].x++;
+            }
+        }
+    };
+
+
+
+/*--------------main()------------------------
+This is the function that makes the game work
+---------------------------------------------*/
+
+function main()
+{
+    //erases the screen
+    ctx.clearRect(0,0,c.width,c.height); 
+
+    //Any changes to numbers
+    if(d==true){ avatar.x += avatar.vx; }
+    if(a==true){ avatar.x += -avatar.vx;}
+    if(w==true){ avatar.y += -avatar.vy;}
+    if(s==true){ avatar.y += avatar.vy; }
+    //Any collision detection 
+    if(avatar.x < 0 + avatar.w/2){avatar.x = 0 + avatar.w/2;}
+    if(avatar.x > c.width + -avatar.w/2){avatar.x = c.width + -avatar.w/2;}
+    if(avatar.y < 0 + avatar.h/2){avatar.y = 0 + avatar.h/2;}
+    if(avatar.y > c.height + -avatar.h/2){avatar.y = c.height + -avatar.h/2;} 
+    //draw the pictures
+    for(var i=0; i<sheep.length; i++)
+        {
+            
+            if(sheep[i].overlaps(avatar))
+                {
+                    console.log(`boop!`)
+                    sheep[i].x = avatar;
+                    
+                }
+            sheep[i].render();
+         }
+         avatar.render();
+}
+ 
+
+//random number generator
+function rand(_low, _high)
+{
+    return Math.random()*(_high - _low) + _low;
+}
+//Converts degrees to radians
+function radians(_deg)
+{
+    return _deg * Math.PI/180
+}
+
+//Converts radians to degrees
+function degrees(_rad)
+{
+    return _rad * 180/Math.PI
+}
+
+
+
+function rand(_low, _high)
+{
+    return Math.random()*(_high - _low) + _low;
+}
+//-------Diagram--------
+//
+//              /|        c = the hypoteneuse
+//            c / |        b = height
+//             /  | b      a = width
+//            /   |        T = arch tangent angle
+//           /T___|
+//             a
+//
+//--------------------------
+
+//To get a and b (displacement) when you know two points
+//  
+//    a = destination.x - starting.x
+//    b = destination.y - starting.y
+//
+//To get the total distance (hypotenuese) between two points
+//    c = Math.sqrt(_a*_a + _b*_b)
+
+//To get the arc tangent angle (labeled T in the diagram)
+//    radians = Math.atan2(b, a)
+
+//To find a and b if you know c and T
+//    a = Math.cos(T) * c
+//    b = Math.sin(T) * c
