44b53300e4e4c0f17d487591b459e338d963c23e
LeviathanR42 <rfrenner@email.neit.edu>
2025-02-20 22:20:40 -0500

    wizardgame

--- GDS_126_Engine/js/GameStates/level1.js
+++ GDS_126_Engine/js/GameStates/level1.js
@@ -59,8 +59,8 @@ var levelItems=new Group();
 levelItems.add([caveBack.grid, ground, plat, cave.grid]);
 
 //Very back background
-var sky = new GameObject({width:canvas.width, height:canvas.height, color:"cyan"})
-sky.img.src = `images/sky.png`
+var sky = new GameObject({width:4096, height:512, color:"cyan"})
+sky.img.src = `images/skyBackground-01.png`
 
 /*
  	//Not used, unless you want a 4th level of paralax
@@ -70,11 +70,11 @@ sky.img.src = `images/sky.png`
 
 //repeating background
 var rbg = new GameObject({x:level.x, y:level.y, width:1024, height:512})
-rbg.img.src=`images/hills.png`
+rbg.img.src=`images/repeatBackground-01.png`
 
 //middleground
 var bg = new GameObject({x:level.x,y:level.y, width:canvas.width*4, height:canvas.height})
-bg.img.src=`images/bgfull.png`
+bg.img.src=`images/foreground-01.png`
 
 /*------------------vvBULLET STUFFvv----------------------*/
 
